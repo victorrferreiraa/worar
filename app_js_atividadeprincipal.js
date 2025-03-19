f8c459f04c02f5def37629c4e2331cc800c3dd26
Victor <victor.redeam@gmail.com>
2025-03-19 20:06:57 -0300

    Projeto inicial

---
+++ app_js_atividadeprincipal.js
@@ -0,0 +1,15 @@
+alert("Seja bem vindo ao jogo do milhão");
+
+let numeroSecreto = 29;
+let numeroInserido = prompt("Escolha um número entre 1 e 30");
+numeroInserido = Number(numeroInserido);
+console.log = numeroInserido
+
+while (numeroInserido != numeroSecreto) {
+    alert("Você errou! tente novamente");
+    numeroInserido = prompt("Escolha um número entre 1 e 30");
+    numeroInserido = Number(numeroInserido);
+}
+
+alert(`Você finalmente acertou! o número correto é ${numeroSecreto}`)
+console.log(numeroSecreto)
